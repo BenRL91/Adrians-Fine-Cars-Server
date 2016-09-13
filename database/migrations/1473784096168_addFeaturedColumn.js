76855f9df5e4faf850a1e79a004eb37ee5379511
Ben Lee <benlee.eelned@gmail.com>
2016-09-13 12:55:44 -0400

    Fix feature column migration

--- database/migrations/1473784096168_addFeaturedColumn.js
+++ database/migrations/1473784096168_addFeaturedColumn.js
@@ -6,7 +6,7 @@ class AddFeaturedColumnSchema extends Schema {
 
   up () {
     this.table('vehicles', (table) => {
-      table.boolean("featured").notNullable().defaultTo(false);
+      table.addColumn("featured").notNullable().defaultTo(false);
     })
   }
 
